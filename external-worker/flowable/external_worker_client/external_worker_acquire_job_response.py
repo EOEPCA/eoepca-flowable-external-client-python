4e790351015dccfe2cb4e953dbf64913ead491ac
Mario Winkler <mario.winkler@dlr.de>
2024-10-28 17:54:16 +0100

    Add get variable by name for external worker job

--- external-worker/flowable/external_worker_client/external_worker_acquire_job_response.py
+++ external-worker/flowable/external_worker_client/external_worker_acquire_job_response.py
@@ -7,3 +7,12 @@ class ExternalWorkerAcquireJobResponse(ExternalWorkerJobResponse):
     def __init__(self, variables: list[EngineRestVariable], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.variables = variables
+        self.job_vars = {}
+        for v in self.variables:
+            self.job_vars[v.name] = v.value        
+
+    def get_variable(self, name: str) -> str | None:
+        if name in self.job_vars:
+            return self.job_vars[name]
+        else:
+            return None        
